53a6f47254cef38ffbf770f3842b56a09a473321
Shihan Chowdhury <hackedgdown@gmail.com>
2023-05-11 16:20:14 +0100

    Create Client Side post and insert data to Server to MongoDB

--- simple-crud-client/src/App.jsx
+++ simple-crud-client/src/App.jsx
@@ -2,10 +2,37 @@ import './App.css'
 
 function App() {
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const user = {email, password};
+    console.log(user);
+
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user),
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err.message))
+  }
+
   return (
     <>
       <div>
         <h1>Simple CRUD</h1>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input type="email" name='email' placeholder='email' /><br />
+            <input type="password" name='password' placeholder='******' /><br />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </div>
 
     </>
