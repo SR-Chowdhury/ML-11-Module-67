d8a62317066356d1a3a03a1bd56de23b71e7b556
Shihan Chowdhury <hackedgdown@gmail.com>
2023-05-11 22:38:45 +0100

    ML-11-Module-67 done

--- simple-crud-server/__CRUD_setup.js
+++ simple-crud-server/__CRUD_setup.js
@@ -32,6 +32,49 @@
  * step 2: const result = await cursor.toArray();
  * step 3: res.send(result)
  * 
+ * 
+ * -------------------------
+ *     EDIT / SINGLE VIEW
+ * -------------------------
+ * ### Server Side
+ * step 1: app.get('/:id', async (req, res) => {}) N>B> funciton must be async
+ * step 2: get id > const id = req.params.id
+ * step 3: run a query > const query = {_id : new ObjectId(id)} N>B> must import ObjectId with new keyword
+ * step 4: const result = await exampleCollection.findOne(query)
+ * step 5: res.send(result)
+ * 
+ * ### Client Side
+ * step 1: <Link to ="`/users/${id}">Edit</Link>
+ * step 2: Create a dynamic route
+ *      2.1: path: '/users/:id',
+ *      2.2: loader: ({params}) => fetch(`url/${params.id})
+ * 
+ * 
+ * ------------------
+ *     UPDATE
+ * ------------------
+ * ### Server Side
+ * step 1: app.put('/:id', async (req, res) => {}) N>B> funciton must be async
+ * step 2: get id > const id = req.params.id
+ * step 3: get body > const user = req.body;
+ * step 4: run a query > const filter = {_id : new ObjectId(id)} N>B> must import ObjectId with new keyword
+ * step 5: create a options > const options = { upsert : true }
+ * step 6: declare the updated value > const updatedUser = {
+ *              $set: {
+ *                  name: user.name,
+ *                  email: user.email,
+ *              }
+ *         }
+ * step 7: const result = await exampleCollection.updateOne(filter, updatedUser, options);
+ * step 8: res.send(result)
+ * 
+ * ### Client Side
+ * step 1: create fetch (`url/${id})
+ * step 2: add second parameter as an object(method, headers, body) 
+ *      2.1: method: 'PUT'
+ *      2.2: headers: { 'collection-type' : 'applicaiton/json' }
+ *      2.3: body: JSON.stringify(user)
+ * 
  * ----------------
  *   DELETE (One)
  * ----------------
@@ -44,7 +87,7 @@
  * 
  * ### Client Side
  * step 1: create fetch
- * step 2: add second parameter as an object(method, headers, body) 
+ * step 2: add second parameter as an object(method) 
  *      2.1: method: 'DELETE'
  *
  */
